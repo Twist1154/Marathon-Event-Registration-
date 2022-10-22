0f522f3c2b2f885f81ceeeaf08edc0820ec094cf
Twist1154 <rethabile1154n@gmail.com>
2022-10-22 21:53:41 +0200

    Signed-off-by:Twist1154 <rethabile1154n@gmail.com>

--- Documents/NetBeansProjects/12.3IDE/23 October project/Disney-PrimaryClient/src/main/java/za/ac/Views/ViewBook.java
+++ Documents/NetBeansProjects/12.3IDE/23 October project/Disney-PrimaryClient/src/main/java/za/ac/Views/ViewBook.java
@@ -1,9 +1,15 @@
 package za.ac.Views;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 /**
  *
  * @author retha
  */
 public class ViewBook {
-    
+    private JFrame mainFrame;
+    private JTable table;
+private JScrollPane scoll;    
 }
