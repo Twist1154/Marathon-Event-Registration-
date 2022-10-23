599aceb0c333b3d9a2dd7d59f38407232c0b598b
Twist1154 <rethabile1154n@gmail.com>
2022-10-23 16:16:17 +0200

    this Admin Model Class Completed Signed-off-by:Twist1154 <rethabile1154n@gmail.com>

--- Documents/NetBeansProjects/12.3IDE/23 October project/Disney-PrimaryClient/src/main/java/za/ac/Models/Admin.java
+++ Documents/NetBeansProjects/12.3IDE/23 October project/Disney-PrimaryClient/src/main/java/za/ac/Models/Admin.java
@@ -5,5 +5,33 @@ package za.ac.Models;
  * @author retha
  */
 public class Admin {
+    private String usn;
+    private String name;
+
+    public Admin(String usn, String name) {
+        this.usn = usn;
+        this.name = name;
+    }
+
+    public String getUsn() {
+        return usn;
+    }
+
+    public void setUsn(String usn) {
+        this.usn = usn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin" + "Usn:" + usn + "Name: " + name;
+    }
     
 }
