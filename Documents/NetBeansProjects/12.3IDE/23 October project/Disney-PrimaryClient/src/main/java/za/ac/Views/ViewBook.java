8cb9f0d1af23e2fdb1d5f29aed561d2aa027989d
Twist1154 <rethabile1154n@gmail.com>
2022-10-23 16:58:29 +0200

    this Admin Model Class Completed Signed-off-by:Twist1154 <rethabile1154n@gmail.com>

--- Documents/NetBeansProjects/12.3IDE/23 October project/Disney-PrimaryClient/src/main/java/za/ac/Views/ViewBook.java
+++ Documents/NetBeansProjects/12.3IDE/23 October project/Disney-PrimaryClient/src/main/java/za/ac/Views/ViewBook.java
@@ -9,7 +9,23 @@ import javax.swing.JTable;
  * @author retha
  */
 public class ViewBook {
+
     private JFrame mainFrame;
     private JTable table;
-private JScrollPane scoll;    
+    private JScrollPane scoll;
+
+    public ViewBook() {
+        mainFrame = new JFrame("View Book");
+
+    }
+
+    public setGUI() {
+
+//        mainFrame.add();
+        mainFrame.pack();
+        mainFrame.setSize(400, 350);
+        mainFrame.setVisible(true);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
 }
